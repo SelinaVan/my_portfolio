c0e4be7758d923147b5100cea807c4a8ec7875f6
ngoc_phuong2512 <phuong@allxone.vn>
2023-08-23 09:20:22 +0700

    add project

--- index.js
+++ index.js
@@ -1,7 +1,118 @@
-import certs from './assets/certificates/cert.json' assert {type: 'json'};
-import projects from './assets/projects/projects.json' assert {type: 'json'};
+const certs =
+    [
+        {
+            "link": "https://www.hackerrank.com/certificates/f86b76dd6dab",
+            "image": "https://firebasestorage.googleapis.com/v0/b/shop24h-7d406.appspot.com/o/images%2FCss.png?alt=media&token=65cbe75a-2c0a-4be1-a61d-a1589ce79f36",
+            "name": "CSS"
+        },
+        {
+            "link": "https://www.hackerrank.com/certificates/edcf0bc7b56c",
+            "image": "https://firebasestorage.googleapis.com/v0/b/shop24h-7d406.appspot.com/o/images%2FReactJs.png?alt=media&token=12cda596-b872-4929-a3a4-8f68a3deb2b4",
+            "name": "ReactJS"
+        },
+        {
+            "link": "https://www.hackerrank.com/certificates/be5dcadc2669",
+            "image": "https://firebasestorage.googleapis.com/v0/b/shop24h-7d406.appspot.com/o/images%2FJavascript%20intermediate.png?alt=media&token=5cf08332-43f3-4a0b-814f-09bc2f55c126",
+            "name": "Javascript (Intermediate)"
+        },
+        {
+            "link": "https://www.hackerrank.com/certificates/6fa140bdfc01",
+            "image": "https://firebasestorage.googleapis.com/v0/b/shop24h-7d406.appspot.com/o/images%2FJavascript%20basic.png?alt=media&token=2e556112-25ee-48b7-9905-c3349d422879",
+            "name": "Javascript (Basic)"
+        }
 
-$(document).ready(function () {
+    ]
+
+const projects = [
+    {
+        "link": "https://demo-shop-frontend.onrender.com/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/shop24h_bd88e963-4441-44f7-9f61-bb5690d85689.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=L4474qgWn7KLpHlpfCxji%2BMENRPDINdwMroQ6UCfRvxQoymiwZmiTp5z7lf0tktk4EmSFW8schaKjhzJLLke5YU%2BWtYUC5diNzSg8BpPPvl%2F2G%2Fs%2FzNn%2BbIys%2F3TOC7VuvX4JKI0QA83pStaW1YNCN4jA%2F2LnWOqBrQveE98lFn47uqsZ3Bl1v3f9enmHlbwzYqN8Gt0QT%2BvKcGditNQhaNWxy%2F4LGAlrGVPN7BtPIpMu83C3xA3mAjZPzK5%2B3tYvhn6A3%2BNcVsZ%2FLPTWoJbYv%2FctHATj3YXqZc1ieLMhfj75EclUrBTr1gDjbJ1fENkZwcVYcTAnbKgRMx88why6w%3D%3D",
+        "title": "Karl Fashion",
+        "name": "E-commerce",
+        "description": "ReactJs"
+    },
+    {
+        "link": "https://calm-cranachan-b1dec4.netlify.app/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/furniture_3f87d7c6-d3c7-419c-b572-b6239eaa2565.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=Wg4dz3iYLVrW9GQtKaQnq034MAfGCaBus1PUKINTz3Ytd2wcTYGW8kfXShAE7raZXEDAUGZt%2FCjObkwcVZU6xRHHWWrVw%2Fo9L8m%2Fqo5FFZAA%2Fq%2FkWAW7eKZGnJhptml6EIfCimH%2Fpz8ObQSaMw%2BmEJZlARZfM7wYuzxPEt%2BoNdGetDSLKyoLjjnF2nsMIhh%2FpIwglJLso0g7Jr9T3gx1j1PE1gsVp79kwAgtMIZ4Q6BIlKwGRgw%2FGo%2Be%2FQcLrANdd4kROvkVu9K517ZhHE3Zm3I7MqgvLGBt9swYVwQKXbOaB7yLfEfAnLatZ%2FWZ4EDCHUXaBQ8ut%2Bxed92djVth4w%3D%3D",
+        "title": "Furniture",
+        "name": "Landing page",
+        "description": "HTML, CSS"
+    },
+    {
+        "link": "https://beautiful-parfait-470847.netlify.app/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/lenlove_8f947c46-d564-44b0-b60a-2a28d44685b1.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=NTjGpu7tf8Ens3WbBOPn6YEqTIFv38AqbRouoQ%2F7KBSaFooJh8QonORpl2gWyE0j71HxhXZ6nUQC7dNrJUdh2BAjllDZmWCKI%2BPZ94fpouzOa5QF9TBcMCkd5rqjYEOWR6fZyognSUCkLwlLx78tqAA9QZDrwln2ILWEfneCBaNToWLdjTIkxH4C4fPjg0orrD2MSOwxl4Ct%2BSW4izdO%2BGu9BqkX3q9rhjgYLTzmbsgSbSAC4wvmrsgJuBK4BVlhsg9zMlcs4NIZcxUZkUsZhiErxazVUXmh0lpI%2FTQxJzloyNqyUOUGLpoHCN5abSdmhh5aJopGNw2QRMyJ6UtwRw%3D%3D",
+        "title": "Eyewear shop",
+        "name": "Landing page",
+        "description": "HTML, CSS"
+    },
+    {
+        "link": "https://timesheet.allxone.vn/coreui-checkintool/#/login",
+        "image": "./images/works/checkin.png",
+        "name": "Check in App",
+        "description": "Angular, CoreUI, Bootstrap, Laravel, PhpMyAdmin, Php",
+        "title": "Check in, manage timesheet, export data, create request, approve request from employees "
+    },
+    {
+        "link": "https://feedback.allxone.vn/",
+        "image": "./images/works/feedback.png",
+        "name": "Feedback App",
+        "description": "MUI, ReactJS, Laravel, PhpMyAdmin, Php",
+        "title": "Create and custom form, collect feedback from user"
+    },
+    {
+        "link": "https://convertmusic.allxone.vn/",
+        "image": "./images/works/convert.png",
+        "name": "Convert music",
+        "description": "Bootstrap, ReactJS, Firebase, Nodejs",
+        "title": "Convert sheet music"
+    },
+    {
+        "link": "https://dashboard-frontend-fvul.onrender.com/dashboard",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/dashboard_8b7e7c65-2473-4738-a8da-2225f54b125f.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=iOKpmkQKeoVXVLtpDI9OEPNlyBnOqMLs%2BcMje3JP8Yy%2F7MvzLEGLF7e1N2C1x2KpbGZ%2F%2Bz7Z8jMhGSfSxPKruqKw6BKik%2Fqi%2BxDw0wYKibYeMNGaQ%2BCXXuCAH3b13SOrvJJZHhYI40IBWGlmjaA45gPk6qCvfrrwMYyCMJIZGbc6bktwwdA8SK2GMHpoPJE4KVpcPlHaUHyPK8786%2FN%2FRmjUREaHbMDa2o0gwgbFwgD1OaUYoxk8Me2h2WL8lCDgM7kjz4xx098cApqX%2B2ZcemXfL7O71638KSOMVjXFNLJ574hwBKylf1Y7HcLGXUBeH%2Bpr43WjoFLfdOu93YQPPg%3D%3D",
+        "title": "Dashboard",
+        "name": "AdminLTE",
+        "description": "ReactJs"
+    },
+    {
+        "link": "https://course365-frontend.onrender.com/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/course365_c86475e1-4bdd-4ed3-abe3-875ce02df2de.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=o%2F0ATMBv1g8uc8cjXcgPDHt6SYe7Z3SYrou8RUhmhLNJEISYWVS8cKZi1WP4Hg0TpR%2FKmnp9K3pHbIm0JgDWy%2FBsPIxpyTM7eSWF%2F05%2FwXn2XQfxjNaY57vYVYfAoMcDhbL8yYkseKQuNfAYx2ID28P205vVmHxS4pU6irQD2tW0XdgRf7LbrOswi4vxTsIcz0XdxjaXPOVn2FzFoi8JW1lypqxLGc2TtSt0BSxBUJZaUHQzEBM%2Fq1ZobuCYg1zbSZY9Ydj6GXqzRnFSp6NMEzBPpwl0mbgyt2PIptzvryzLEMoVy5ir4W%2FCfNVtrKUFu265GAa0Yt6FjHJP%2FHBhbA%3D%3D",
+        "title": "Course 365",
+        "name": "Landing page",
+        "description": "ReactJs"
+    },
+    {
+        "link": "https://pizza365.onrender.com/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/pizza365_c48e3c3e-990a-4821-8c53-ec62548bebdc.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=Yy1QGrXC2MDFloVCnLTeEBz27AtL7jBE0aSHctTViRYRAIGV77vi84ld63MH8i7G6k6VMvSH9Kq4rGHaulQnNr2aRHDkoi9zfy%2FKgCdcW8nffQWA5TFPeZlNJb4vomysl8cB9%2F4ucs49KwMsRDsaKjb%2FZxHNCoYsM%2F90OfTSew9FIi5xudv%2FHKIvjC6Mmvn6iELvTAW2ICzEgSE6%2FGKMNeI%2BHwdvj15%2FRfZ4NJWyDQamcciMZv%2FUa%2BdgHz5qj7%2FfbLVjkeQRnqCscveECyoTZilmHuvbUCmwOMDfCP42JlXQ3svVPXVdTHQ80KlEKVCykn3bCmmre%2FjGYogjyUwCNQ%3D%3D",
+        "title": "Pizza 365",
+        "name": "Landing page",
+        "description": "ReactJs"
+    },
+    {
+        "_id": { "$oid": "643ab67cdfdbdb078c2b2d5d" },
+        "link": "https://weather-app-moak.onrender.com/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/weather-app_e2827b4a-d755-47cd-9351-257d62e7b72b.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=OPOfx0FTvxHp5TjcVq58kKBy%2Bf7KViqOQSe%2Bc7kipengUaKhkkQOw6KngyDPAvNcvpf1c6TZkm7zFEszi83juxLT66nhfBr3mKhWsRih9BNodFRDDdIvGyy4Jwl%2FFyFkV3mPGm8p4hiLDEBcLSPCu4C3g577GcF0dpH53W%2FV5k4f2yqP4SetK7sC1d8v4y8OKSRuNSk6nsVJ3dSk%2BG5vGSGfm3ZzWtWSpI30JjkaxMWNagiLFMki5F6QHWu71P7tAhotbjsjMZbb168UVBn97SJWyQQTN9Zg3LAGPJcfvbhO0oD6h%2Bwt0s6C74zT0yGAWkpHV14HybTkGyuQYMP6RQ%3D%3D",
+        "title": "Weather app",
+        "name": "Landing page",
+        "description": "ReactJs"
+    },
+    {
+        "_id": { "$oid": "643b94f953d88adb529c40d1" },
+        "link": "https://lucky-dice.onrender.com/",
+        "image": "https://storage.googleapis.com/shop24h-7d406.appspot.com/images/luckydice_8c1fcc05-8972-466f-9b2f-3c7c5d876050.png?GoogleAccessId=firebase-adminsdk-rygp4%40shop24h-7d406.iam.gserviceaccount.com&Expires=1742169600&Signature=ID0WGf8vs8gk3EWFkxv8%2BYdAgYtsbEmCHqtOVe%2BX3sKA%2F4ciBbBPVn1eF6xhfMlzKr5X49GxWrALAX%2BoFLHJ1MrrRJmUxcZKJx8ulyXZ%2BorerSfQHUH5os9AafoG7C3tTvIfjPZB%2FPxPNcMMtMXjdnU8VoF5bYjCfjI5OBTvcPkBGmbGnsCxpDWSBVFAdd6FF3f0n3hS7lCY0VC4DkFQUJtyA5n7ghYmhh9ZCAnAO9U%2BXHyTezxKaDkxn0cQRQkm8RLoiHG1CPB9IkogmAKquPCTEuJPvlcFtnQERMMwZFCo%2BVk8TRGuDDjAs0p3BP6wubF4QB%2BxwQJP6240IOeDLg%3D%3D",
+        "title": "Lucky dice",
+        "name": "Game lucky dice",
+        "description": "NodeJs, SSR"
+    },
+    {
+        "link": "https://nextjs-frontend-kpgs-6c0cs6ktm-selinavan.vercel.app/",
+        "image": "https://firebasestorage.googleapis.com/v0/b/shop24h-7d406.appspot.com/o/images%2Fnextjs.png?alt=media&token=2eb7b229-adbb-4ba6-8289-54d0fe3a9d88",
+        "name": "Blog of Future",
+        "description": "NextJs, Tailwind, prisma",
+        "title": "Blog of Future"
+    },
+
+]
+document.addEventListener('DOMContentLoaded', (event) => {
     // const API_URL = 'https://portfolio-backend-kdud.onrender.com/';
     renderProject(projects);
     renderCertificate(certs);
@@ -151,4 +262,5 @@ $(document).ready(function () {
     //         }
     //     })
     // }
+
 })
